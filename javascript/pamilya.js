50c8433a0c05639fe00f73bccb44d4720b95e7c8
Kelly Gabot <86221756+kellyG23@users.noreply.github.com>
2024-04-21 20:01:51 +0800

    tapos ang buhay

---
+++ javascript/pamilya.js
@@ -0,0 +1,18 @@
+const targetWords = [
+  "anak",
+  "apo",
+  "biyenan",
+  "bilas",
+  "balae",
+  "ulila",
+  "panganay",
+  "bunso",
+  "hipag",
+  "bayaw",
+  "kakambal",
+  "manugang",
+  "ninuno",
+  "pinsan",
+  "pamangkin",
+  "nuno",
+];
